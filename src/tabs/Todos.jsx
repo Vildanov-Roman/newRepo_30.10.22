c45f231adb0b991978a8b9b8ce5014e306f8b113
Roman Vildanov <vildanovv.rr@gmail.com>
2022-10-30 20:47:25 +0200

    we tryed

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+// import { nanoid } from 'nanoid';
+// import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
   render() {
